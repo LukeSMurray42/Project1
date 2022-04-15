d5d6bb0b873e4df4b3f7679b35338d319894fd9c
chris <cmor5434@uni.sydney.edu.au>
2022-04-16 04:25:29 +1000

    adding comments to original code

--- hangman_originals/hangman-master/hangman.py
+++ hangman_originals/hangman-master/hangman.py
@@ -1,33 +1,35 @@
+# Importing Modules
+
 import random
 
 import flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialising Flask
 app = flask.Flask(__name__)
-#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False # Debug
 
-# Database
-
+# Database Setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hangman.db'
 db = SQLAlchemy(app)
+
 
 # Model
 
 def random_pk(): #Random Game ID
-    return random.randint(1e9, 1e10)
+    return random.randint(1e9, 1e10) # Returns random Game ID for unique identification in the database formed
 
 def random_word():
-    words = [line.strip() for line in open('words.txt') if len(line) > 10] # Defines how long a word must be - we can change the difficulty here
+    words = [line.strip() for line in open('words.txt') if len(line) > 10] # Defines how long a word must be - the game's difficulty can be changed here
     return random.choice(words).upper()
 
-class Game(db.Model):
-    pk = db.Column(db.Integer, primary_key=True, default=random_pk)
-    word = db.Column(db.String(50), default=random_word)
-    tried = db.Column(db.String(50), default='')
-    player = db.Column(db.String(50))
+class Game(db.Model): # Creation of class to store the data of every game played into one database
+    pk = db.Column(db.Integer, primary_key=True, default=random_pk) # Game ID (acquired from random_pk function)
+    word = db.Column(db.String(50), default=random_word) # Current (correct) word for the current game (acquired from random_word function)
+    tried = db.Column(db.String(50), default='') # Letters guessed during gameplay (includes both correct and incorrect letters - hence errors made can be found here as well)
+    player = db.Column(db.String(50)) # Player name (initialised in __init__)
 
     def __init__(self, player):
-        self.player = player
+        self.player = player # Storing player name into database
 
     @property
     def errors(self):
@@ -35,18 +37,18 @@ class Game(db.Model):
 
     @property
     def current(self):
-        return ''.join([c if c in self.tried else '_' for c in self.word]) # Corresponds to word shown on screen
+        return ''.join([c if c in self.tried else '_' for c in self.word]) # Corresponds to current word shown on screen
 
     @property
     def points(self):
-        return 100 + 2*len(set(self.word)) + len(self.word) - 10*len(self.errors) # POINT SYSTEM - based on: number of unique letters in word, overall length of word, number of errors made (TODO: show on screen)
-
+        return 100 + 2*len(set(self.word)) + len(self.word) - 10*len(self.errors) # POINT SYSTEM - based on: number of unique letters in word, overall length of word, number of errors made
+    
     # Play
 
     def try_letter(self, letter):
-        if not self.finished and letter not in self.tried: # already checks for duplicates here
-            self.tried += letter
-            db.session.commit()
+        if not self.finished and letter not in self.tried: # Checks for duplicates here
+            self.tried += letter # Adds letter to database
+            db.session.commit() # Updates Database
 
     # Game status
 
@@ -74,11 +76,11 @@ def home():
 
 @app.route('/play')
 def new_game(): # Initialises new game, adds data (players name) into database
-    player = flask.request.args.get('player')
-    game = Game(player)
-    db.session.add(game)
+    player = flask.request.args.get('player') # Reads player name from user input
+    game = Game(player) # Creates new instance of Game
+    db.session.add(game) # Adds and commits new game created into database
     db.session.commit()
-    return flask.redirect(flask.url_for('play', game_id=game.pk))
+    return flask.redirect(flask.url_for('play', game_id=game.pk)) # Redirects to 'play' function
 
 @app.route('/play/<game_id>', methods=['GET', 'POST'])
 def play(game_id):
@@ -89,7 +91,7 @@ def play(game_id):
         if len(letter) == 1 and letter.isalpha(): # Checking for a valid input (only 1 LETTER is given)
             game.try_letter(letter)
 
-    if flask.request.is_xhr: # This 'is_xhr' is the part that stops the program from working. I finally got it to work for my computer by downloading an earlier version of flask (1.1.2) and Werkzeug (0.16.1).
+    if flask.request.is_xhr: # This 'is_xhr' is the part that stops the program from working on current software. Must download an earlier version of flask (1.1.2) and Werkzeug (0.16.1).
         return flask.jsonify(current=game.current,
                              errors=game.errors,
                              finished=game.finished)
